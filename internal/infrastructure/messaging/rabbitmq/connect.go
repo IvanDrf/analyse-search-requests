315c7c467bf00d30c5c9c430824422c35a5dac4e
IvanDrf <ivandrgmr@gmail.com>
2026-05-24 16:15:52 +0300

    [dev]: add rabbitmq consumer

--- internal/infrastructure/messaging/rabbitmq/connect.go
+++ internal/infrastructure/messaging/rabbitmq/connect.go
@@ -7,11 +7,24 @@ import (
 	"github.com/rabbitmq/amqp091-go"
 )
 
-func Connect(config *config.RabbitMQConfig) *amqp091.Connection {
+func Connect(config *config.RabbitMQConfig) (*amqp091.Connection, *amqp091.Channel, *amqp091.Queue) {
 	conn, err := amqp091.Dial(config.DSN())
 	if err != nil {
 		log.Fatalf("can't connect to rabbitmq, error=%s", err)
 	}
 
-	return conn
+	ch, err := conn.Channel()
+	if err != nil {
+		conn.Close()
+		log.Fatalf("can't declare channel, error=%s", err)
+	}
+
+	queue, err := ch.QueueDeclare(config.Queue, true, false, false, false, nil)
+	if err != nil {
+		ch.Close()
+		conn.Close()
+		log.Fatalf("can't declare queue, error=%s", err)
+	}
+
+	return conn, ch, &queue
 }
---
+++ internal/infrastructure/messaging/rabbitmq/consumer.go
@@ -0,0 +1,97 @@
+package rabbitmq
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"sync"
+
+	"github.com/IvanDrf/analyse-search-requests/internal/domain/models"
+	"github.com/IvanDrf/analyse-search-requests/internal/domain/ports/service"
+	"github.com/rabbitmq/amqp091-go"
+)
+
+type searchConsumer struct {
+	conn  *amqp091.Connection
+	ch    *amqp091.Channel
+	queue *amqp091.Queue
+
+	searchService service.SearchService
+
+	workers int
+}
+
+func NewSearchConsumer(
+	conn *amqp091.Connection, ch *amqp091.Channel,
+	queue *amqp091.Queue, workers int,
+) *searchConsumer {
+	return &searchConsumer{
+		conn:    conn,
+		ch:      ch,
+		queue:   queue,
+		workers: workers,
+	}
+}
+
+func (c *searchConsumer) Close() {
+	c.ch.Close()
+	c.conn.Close()
+}
+
+func (c *searchConsumer) ReadMessages(ctx context.Context) error {
+	delivery, err := c.ch.Consume(c.queue.Name, "", false, false, false, false, nil)
+	if err != nil {
+		return models.Error{
+			Message: fmt.Sprintf("can't start reading messages, error=%s", err),
+		}
+	}
+
+	wg := new(sync.WaitGroup)
+	for range c.workers {
+		wg.Add(1)
+		go c.worker(ctx, delivery)
+	}
+
+	<-ctx.Done()
+	wg.Wait()
+	return nil
+}
+
+func (c *searchConsumer) worker(ctx context.Context, delivery <-chan amqp091.Delivery) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+
+		case message, ok := <-delivery:
+			if !ok {
+				return
+			}
+
+			if err := c.processMessage(ctx, message); err != nil {
+				slog.Error("", slog.String("error", err.Error()))
+			}
+		}
+	}
+}
+
+func (c *searchConsumer) processMessage(ctx context.Context, message amqp091.Delivery) error {
+	search := models.Message{}
+	if err := json.Unmarshal(message.Body, &search); err != nil {
+		message.Ack(false)
+		return models.Error{
+			Message: fmt.Sprintf("can't parse incoming message from queue, error=%s", err),
+		}
+	}
+
+	if err := c.searchService.SaveSearch(ctx, &search); err != nil {
+		message.Nack(false, true)
+		return models.Error{
+			Message: fmt.Sprintf("can't save new search error=%s", err),
+		}
+	}
+
+	message.Ack(false)
+	return nil
+}
