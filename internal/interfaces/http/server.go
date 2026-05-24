51ad2266a38a4fda110c1072b5b1dbfeaf4a10fd
IvanDrf <ivandrgmr@gmail.com>
2026-05-24 15:43:33 +0300

    [dev]: add Start method for server

--- internal/interfaces/http/server.go
+++ internal/interfaces/http/server.go
@@ -1,12 +1,17 @@
 package http
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 type searchServer struct {
 	host string
 	port int
 
 	mux      http.ServeMux
+	server   http.Server
 	handlers *handlers
 }
 
@@ -16,6 +21,7 @@ func NewSearchServer(host string, port int, handlers *handlers) *searchServer {
 		port: port,
 
 		mux:      *http.NewServeMux(),
+		server:   http.Server{},
 		handlers: handlers,
 	}
 }
@@ -25,4 +31,12 @@ func (s *searchServer) RegisterRoutes() {
 	s.mux.HandleFunc("DELETE /api/v1/bad", s.handlers.deleteBadWord)
 
 	s.mux.HandleFunc("GET /api/v1/searches", s.handlers.findMostPopularSearches)
+}
+
+func (s *searchServer) Start() {
+	s.server.Addr = fmt.Sprintf("%s:%d", s.host, s.port)
+
+	if err := s.server.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatalf("can't start http server, error=%s", err)
+	}
 }
